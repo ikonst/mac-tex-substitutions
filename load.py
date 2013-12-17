2128f7b88ea068fe2c7a36a450cd0a87024680c0
Ilya Konstantinov <ilya.konstantinov@gmail.com>
2013-12-17 20:41:05 +0200

    Try to fix the code to work with 10.8 and higher (tested on Maverick). While I still haven't managed to have the new preferences apply (the prefpane resets them), I've succeeded in having the prefpane show the new settings at least momentarily, by sending the new IMKUserDictionaryDidChangeNotification notification. This still isn't finished. Also, improved code style -- removed semicolons, split long lines etc.

--- load.py
+++ load.py
@@ -1,47 +1,97 @@
 #!/usr/bin/python2.6
-import json;
-import sys;
-from Foundation import NSUserDefaults, NSMutableDictionary, NSDistributedNotificationCenter
+"""This script imports text substitutions into OS X preferences."""
+import json
+import sys
+import time
+from Foundation import (
+    NSUserDefaults,
+    NSMutableDictionary,
+    NSDistributedNotificationCenter
+)
+from CoreFoundation import (
+    CFPropertyListCreateDeepCopy,
+    kCFPropertyListMutableContainersAndLeaves
+)
 
 if len(sys.argv) < 2:
-	sys.exit("Usage: load.py [map.json]")
+    sys.exit("Usage: load.py [map.json]")
 
-# Using the same JSON format as https://github.com/dwaite/mac-text-substitutions
-f = open(sys.argv[1], 'r');
-importedData = json.load(f);
-f.close();
+# Using the same JSON format as
+# https://github.com/dwaite/mac-text-substitutions
+f = open(sys.argv[1], 'r')
+importedData = json.load(f)
+f.close()
 
-# reset counters
-updated = added = 0
+updated = added = 0 # reset counters
 
-# update entries that already exist
-prefs = NSUserDefaults.standardUserDefaults();
-dict = prefs.persistentDomainForName_('.GlobalPreferences');
-items = dict.objectForKey_('NSUserReplacementItems');
+# This code reads and modifies OS X preferences.
+prefs = NSUserDefaults.standardUserDefaults()
+globalPrefs = prefs.persistentDomainForName_('.GlobalPreferences')
+
+# Since NSUserDefaults returns immutable objects, we create
+# a deep(!) mutable copy, and then assign this copy
+# at the end of the script.
+globalPrefs = CFPropertyListCreateDeepCopy(
+    None, globalPrefs, kCFPropertyListMutableContainersAndLeaves)
+
+# Before 10.8, this was called NSUserReplacementItems.
+# From 10.8, this is called NSUserDictionaryReplacementItems.
+itemsName = 'NSUserReplacementItems'
+items = globalPrefs.objectForKey_(itemsName)
+if items == None:
+    itemsName = 'NSUserDictionaryReplacementItems'
+    items = globalPrefs.objectForKey_(itemsName)
+    if items == None:
+        sys.exit('Error: The global preferences do not contain '
+                 'a replacement items list. '
+                 'Is this OS version supported?')
+
+# Update entries that already exist
 for item in items:
-	replace = item.valueForKey_('replace')
-	if importedData.has_key(replace):
-		importedItem = importedData[replace]
-		item.setObject_forKey_(importedItem['enabled'], 'on');
-		item.setObject_forKey_(importedItem['replacement'], 'with');
-		del importedData[replace]
-		updated += 1
+    replace = item.valueForKey_('replace')
+    if importedData.has_key(replace):
+        importedItem = importedData[replace]
+        item.setObject_forKey_(importedItem['enabled'], 'on')
+        item.setObject_forKey_(importedItem['replacement'], 'with')
+        del importedData[replace]
+        updated += 1
 
-# add new entries
+# Add new entries
 for key, importedItem in importedData.iteritems():
-	item = NSMutableDictionary.dictionary();
-	item.setObject_forKey_(importedItem['enabled'], 'on');
-	item.setObject_forKey_(key, 'replace');
-	item.setObject_forKey_(importedItem['replacement'], 'with');
-	items.addObject_(item);
-	added += 1
+    item = NSMutableDictionary.dictionary()
+    item.setObject_forKey_(importedItem['enabled'], 'on')
+    item.setObject_forKey_(key, 'replace')
+    item.setObject_forKey_(importedItem['replacement'], 'with')
+    items.addObject_(item)
+    added += 1
 
-prefs.setPersistentDomain_forName_(dict, '.GlobalPreferences');
+# Update the preferences
+prefs.setPersistentDomain_forName_(globalPrefs, '.GlobalPreferences')
+prefs.synchronize()
 
-NSUserDefaults.resetStandardUserDefaults();
+# Release the preferences from memory
+NSUserDefaults.resetStandardUserDefaults()
 
-# Apply the changes immediately (the preference pane sends this notification)
-NSDistributedNotificationCenter.defaultCenter().postNotificationName_object_('NSSpellServerReplacementsChanged', None);
+# Build notification userInfo object
+userInfo = {}
+userInfoEntries = []
+for item in items:
+    userInfoEntries.append({
+                           'shortcut': item.valueForKey_('replace'),
+                           'phrase': item.valueForKey_('with'),
+                           'timestamp': int(time.time())
+                           })
+userInfo = {
+    'IMKUserDictionaryUserInfoCurrentEntriesKey': userInfoEntries
+}
+
+# Makes our changes to apply immediately on running processes.
+# It's undocumented but this is the notification that the prefpane
+# sends out when you make changes.
+nc = NSDistributedNotificationCenter.defaultCenter()
+nc.postNotificationName_object_('NSSpellServerReplacementsChanged', None)
+nc.postNotificationName_object_userInfo_(
+   'IMKUserDictionaryDidChangeNotification', None, userInfo)
 
 print repr(added) + " substitutions added."
 print repr(updated) + " substitutions updated."
